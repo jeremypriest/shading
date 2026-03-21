ccce5270e25217438811c1eab99ff8057f848c6a
Jeremy Priest <jeremy.m.priest@gmail.com>
2026-03-21 16:54:34 -0500

    Set resolution on global octree dynamically and instantiate on function call

--- main.f90
+++ main.f90
@@ -4,32 +4,45 @@ program shading
     implicit none
 
     interface
-        subroutine build_octree(point_cloud, &
-                                n_points) &
-                                bind(C, name='interop_build_octree')
+        subroutine interop_build_octree(point_cloud, &
+                                        n_points, &
+                                        resolution) &
+        bind(C, name='interop_build_octree')
             import :: c_float, c_int
-            integer(kind=c_int), value :: n_points
-            real(kind=c_float), dimension(3, n_points) :: point_cloud
+            integer(kind=c_int), value, intent(in) :: n_points
+            real(kind=c_float), dimension(3, n_points), intent(in) :: point_cloud
+            real(kind=c_float), value, intent(in) :: resolution
         end subroutine
-        subroutine check_ray(ray) &
-                             bind(C, name='interop_check_ray')
+        subroutine get_intersecting_voxel_elements(ray) &
+        bind(C, name='interop_get_intersecting_voxel_elements')
             import :: c_float
-            real(kind=c_float), dimension(3) :: ray
+            real(kind=c_float), dimension(3), intent(in) :: ray
         end subroutine
     end interface
   
     real(kind=c_float), dimension(:, :), allocatable :: pt_cloud
+    integer, parameter :: n_points = 1000000
+    real(kind=c_float), parameter :: resolution = 0.1
     real(kind=c_float), dimension(3) :: ray_to_check
 
-    allocate(pt_cloud(3, 100))
-
+    allocate(pt_cloud(3, n_points))
     call random_init(repeatable=.true., image_distinct=.false.)
     call random_number(pt_cloud)
 
-    call build_octree(pt_cloud, size(pt_cloud, 2))
+    call build_octree(pt_cloud, resolution)
 
     ray_to_check = [1.0, 2.0, 3.0]
-    call check_ray(ray_to_check)
+    call get_intersecting_voxel_elements(ray_to_check)
 
     deallocate(pt_cloud)
+
+    contains
+        subroutine build_octree(point_cloud, resolution)
+            real(kind=c_float), dimension(:, :), allocatable, intent(in) :: point_cloud
+            real(kind=c_float), intent(in) :: resolution
+
+            call interop_build_octree(point_cloud, &
+                                      size(point_cloud, 2), &
+                                      resolution)
+        end subroutine
 end program
--- octree_interop.cpp
+++ octree_interop.cpp
@@ -3,22 +3,22 @@
 #include <pcl/point_cloud.h>
 #include <pcl/octree/octree_search.h>
 
-float resolution = 128.0f;
-pcl::octree::OctreePointCloudSearch<pcl::PointXYZ> tree_search (resolution);
-pcl::octree::OctreePointCloud<pcl::PointXYZ> tree (resolution);
+// Globals
+pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>::Ptr octree; // Pointer to Octree
+const Eigen::Vector3f zero_vector = {0.0f, 0.0f, 0.0f};
 
 // Refs:
 // - 2D array interop: https://stackoverflow.com/a/67986571
 // - PCL Octree: https://pointclouds.org/documentation/tutorials/octree.html#octree-search
 
 extern "C" void interop_build_octree(float* point_cloud,
-                                     int n_points) {
+                                     int n_points,
+                                     float resolution) {
     // Build the point cloud that we'll build the Octree with.
     pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
     cloud->width = n_points;
     cloud->height = 1;
     cloud->points.resize(n_points);
-
     for (int i_point = 0; i_point < n_points; i_point++) {
         (*cloud)[i_point].x = point_cloud[i_point * 3 + 0];
         (*cloud)[i_point].y = point_cloud[i_point * 3 + 1];
@@ -26,54 +26,28 @@ extern "C" void interop_build_octree(float* point_cloud,
     }
 
     // Build the Octree and set it as a global variable.
-    tree_search.setInputCloud(cloud);
-    tree_search.addPointsFromInputCloud();
+    std::cout << "Resolution = " << resolution << std::endl;
+    octree.reset(new pcl::octree::OctreePointCloudSearch<pcl::PointXYZ>(resolution));
+    octree->setInputCloud(cloud);
+    octree->addPointsFromInputCloud();
 
-    pcl::PointXYZ searchPoint;
-    searchPoint.x = (*cloud)[0].x;
-    searchPoint.y = (*cloud)[0].y;
-    searchPoint.z = (*cloud)[0].z;
-
-    std::cout << searchPoint.x << std::endl;
-    std::cout << searchPoint.y << std::endl;
-    std::cout << searchPoint.z << std::endl;
-
-    // Neighbors within voxel search
-    std::vector<int> pointIdxVec;
-    if (tree_search.voxelSearch(searchPoint, pointIdxVec)) {
-        std::cout << "Neighbors within voxel search at (" << searchPoint.x 
-                                                   << " " << searchPoint.y 
-                                                   << " " << searchPoint.z << ")" 
-                                                   << std::endl;
-        
-        for (std::size_t i = 0; i < pointIdxVec.size(); ++i) {
-            std::cout << "    " << (*cloud)[pointIdxVec[i]].x 
-                        << " " << (*cloud)[pointIdxVec[i]].y 
-                        << " " << (*cloud)[pointIdxVec[i]].z << std::endl;
-        }
-    }
-
-    tree.setInputCloud(cloud);
-    tree.addPointsFromInputCloud();
-    std::cout << tree.getLeafCount() << std::endl;
-    // Eigen::Vector3f min_pt;
-    // Eigen::Vector3f max_pt;
-    // tree.getVoxelBounds(min_pt, max_pt);
+    // See how many leaves are in the Octree.
+    std::cout << "Number of octree leaves = " << octree->getLeafCount() << std::endl;
 }
 
-extern "C" void interop_check_ray(float* ray) {
-    std::cout << ray[0] << std::endl;
-    std::cout << ray[1] << std::endl;
-    std::cout << ray[2] << std::endl;
+extern "C" void interop_get_intersecting_voxel_elements(float* ray) {
+    std::cout << "Number of octree leaves = " << octree->getLeafCount() << std::endl;
 
-    // auto const ray_array = std::to_array({ray[0], ray[1], ray[2]});
-    // auto const intersected_regions = octree.RayIntersectedAll(
-    //     zero_point,
-    //     ray_array
-    // );
-
-    // std::cout << intersected_regions.size() << std::endl;
-    // for(long unsigned int i; i < intersected_regions.size(); i++) {
-    //     std::cout << intersected_regions[i] << std::endl;
+    std::vector<int> intersected_voxel_point_indices;
+    int n_intersected_voxels = octree->getIntersectedVoxelIndices(
+        zero_vector,
+        Eigen::Vector3f{ray[0], ray[1], ray[2]},
+        intersected_voxel_point_indices
+    );
+    std::cout << "Number of voxels intersected by ray = " << n_intersected_voxels << std::endl;
+    std::cout << "Number of points in voxels intersected by ray = " << intersected_voxel_point_indices.size() << std::endl;
+    // std::cout << "Intersected voxel point indices:" << std::endl;
+    // for (long unsigned int i_point = 0; i_point < intersected_voxel_point_indices.size(); i_point++) {
+    //     std::cout << intersected_voxel_point_indices[i_point] << std::endl;
     // }
 }
