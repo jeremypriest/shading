a609e7f9d41d4dbe188178d804bfd351b79bf8aa
Jeremy M. Priest <jeremy.m.priest@nasa.gov>
2026-03-23 13:24:15 -0500

    Build on FSL. Use VCPKG to install PCL.

--- set_env.sh
+++ set_env.sh
@@ -1,12 +1,26 @@
+# FSL modules
+module load intel-oneapi/2023.2.4
+module load python/3.12
+
 # Make venv
 python3 -m venv .venv
 source .venv/bin/activate
-
-# Use gcc suite of compilers
-export FC=gfortran
-export CC=gcc-15
-export CXX=g++-15
+pip install --upgrade pip
 
 # Meson
 pip install meson cmake ninja
 # brew install pkg-config
+
+# Use Intel suite of compilers
+export FC=ifort
+export CC=icx
+export CXX=icpx
+
+# Build PCL
+git clone https://github.com/microsoft/vcpkg.git
+cd vcpkg && ./bootstrap-vcpkg.sh
+export VCPKG_ROOT=`pwd`
+export PATH=$VCPKG_ROOT:$PATH
+cd ..
+vcpkg install pcl --x-install-root=pcl
+export PKG_CONFIG_PATH=`pwd`/vcpkg/pcl/x64-linux/lib/pkgconfig:$PKG_CONFIG_PATH
