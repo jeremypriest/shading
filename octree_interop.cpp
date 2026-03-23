a46383b1704cdea71d002381a6feefc9b9bda8db
Jeremy M. Priest <jeremy.m.priest@nasa.gov>
2026-03-23 18:58:47 -0500

    valgrind shows we don't need to destroy the octree

--- octree_interop.cpp
+++ octree_interop.cpp
@@ -51,7 +51,3 @@ extern "C" void interop_get_intersecting_voxel_elements(float* ray) {
     //     std::cout << intersected_voxel_point_indices[i_point] << std::endl;
     // }
 }
-
-extern "C" void interop_destroy_octree() {
-    octree.reset();
-}
