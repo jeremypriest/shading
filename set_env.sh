6c9c52dfb1d1f3ce3584687f8bace03dae0f55f9
Jeremy M. Priest <jeremy.m.priest@nasa.gov>
2026-03-23 18:59:34 -0500

    Build PCL using vcpkg

--- set_env.sh
+++ set_env.sh
@@ -12,9 +12,15 @@ pip install meson cmake ninja
 # brew install pkg-config
 
 # Use Intel suite of compilers
+module load intel-oneapi/2023.2.4
 export FC=ifort
 export CC=icx
 export CXX=icpx
+
+# module load gcc/14.2.0
+# export FC=gfortran
+# export CC=gcc
+# export CXX=g++
 
 # Build PCL
 git clone https://github.com/microsoft/vcpkg.git
@@ -22,5 +28,6 @@ cd vcpkg && ./bootstrap-vcpkg.sh
 export VCPKG_ROOT=`pwd`
 export PATH=$VCPKG_ROOT:$PATH
 cd ..
-vcpkg install pcl --x-install-root=pcl
-export PKG_CONFIG_PATH=`pwd`/vcpkg/pcl/x64-linux/lib/pkgconfig:$PKG_CONFIG_PATH
+vcpkg install pcl --x-install-root=pcl # --binarysource=clear
+export PKG_CONFIG_PATH=`pwd`/pcl/x64-linux/lib/pkgconfig:$PKG_CONFIG_PATH
+# export PKG_CONFIG_PATH=`pwd`/pcl/x64-linux/debug/lib/pkgconfig:$PKG_CONFIG_PATH
